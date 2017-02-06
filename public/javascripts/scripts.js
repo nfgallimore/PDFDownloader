c550ea0788700496a818b1166667a028fa2030ba
Nicholas Gallimore <nfg3@zips.uakron.edu>
2017-02-06 12:45:59 -0500

    renamed subStr to path and inserted /link/ at head of string

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -11,8 +11,8 @@ app.run([
             var url = $("#user_entry").val();
             document.getElementById("para").innerText = url;
             var index = url.indexOf("github.com/") + 11;
-            var subStr = url.substring(index, url.length);
-            $.get('/link/' + subStr, function(data, status){
+            var path = "/link/" + url.substring(index, url.length);
+            $.get(path, function(data, status){
                 console.log(data, status);
             });
             $.get('/users', function(data, status) {
