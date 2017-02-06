5a8377ce54e5c1453906483fe2d60fb6a044ba82
Nicholas Gallimore <nfg3@zips.uakron.edu>
2017-02-06 09:34:38 -0500

    Changed routes to sendStatus(200) instead of deprecated send(200)

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,7 @@ router.get('/link/:user/:repo', function(req, res) {
   exec("rm -rf master.zip");
   exec("wget https://github.com/" + req.params.user + "/" + req.params.repo + "/archive/master.zip ; unzip master.zip");
   exec("./main " + req.params.repo);
-  res.send(200);
-
+  res.sendStatus(200);
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.sendStatus(200);
 });
 
 module.exports = router;
